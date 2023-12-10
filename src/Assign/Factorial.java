2b7a207be39fcf687b9bf2b22efb346d5d8762c5
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-12-10 19:32:33 +0530

    factorial-recursion

---
+++ src/Assign/Factorial.java
@@ -0,0 +1,22 @@
+package Assign;
+import java.util.*;
+//Q1. Write a Java program to find the factorial of a number using recursion.
+
+public class Factorial {
+ public static void main(String[] args) {
+     @SuppressWarnings("resource")
+	Scanner sc=new Scanner(System.in);
+     System.out.println("\n Enter Number:");
+     int n=sc.nextInt();
+     int fact=FactM(n);
+     System.out.println("Factorial of "+n+" is:"+fact);
+ }
+ public static int FactM(int n){
+     if(n>=1){
+         return n*FactM(n-1);
+     }
+     else{
+         return 1;
+     }
+ }
+}
